6786fc6ed8b60ad7a4e24c3f2cd9f75fed04c292
gazinnur <gazinnur@mail.ru>
2022-11-03 04:03:15 +0300

    add exp 001 нахождение средней цифры трехзначного числа

---
+++ Homework8/Exp001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ.
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int amount = num.ToString().Length;
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
+    else
+ { int centrnum = (num / 10) % 10;
+    Console.WriteLine(centrnum);
+ }
