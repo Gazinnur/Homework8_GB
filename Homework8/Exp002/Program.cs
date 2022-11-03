8ddb56ee71179dd47cd78fbc99104aea0b4e0c30
gazinnur <gazinnur@mail.ru>
2022-11-03 04:38:43 +0300

    add exp 003 нахождение третье  цифры введенного числа

---
+++ Homework8/Exp002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int amount = num.ToString().Length;
+if (amount < 3)
+{
+    Console.WriteLine("нет третьей цифры");
+}
+else
+{ int c = 1;
+    for (int i = amount; i > 3; i--)
+    { c = c * 10; }
+    int thirdnum = (num / c) % 10;
+    Console.WriteLine(thirdnum);
+}
