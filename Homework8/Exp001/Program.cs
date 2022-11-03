b7c9479478c20d5b5519467ff09ca76ab4770e2e
gazinnur <gazinnur@mail.ru>
2022-11-03 20:54:49 +0300

    Исправление exp002

--- Homework8/Exp001/Program.cs
+++ Homework8/Exp001/Program.cs
@@ -2,12 +2,15 @@
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ.
 Console.WriteLine("Введите трехзначное число");
 int num = int.Parse(Console.ReadLine());
-int amount = num.ToString().Length;
-if (amount < 3 || amount > 3)
+num = Math.Abs(num);
+//int amount = num.ToString().Length;
+// if (amount < 3 || amount > 3)
+if (num > 999 || num < 100)
 {
     Console.WriteLine("Введено не трехзначное число");
 }
-    else
- { int centrnum = (num / 10) % 10;
+else
+{
+    int centrnum = (num / 10) % 10;
     Console.WriteLine(centrnum);
- }
+}
--- Homework8/Exp002/Program.cs
+++ Homework8/Exp002/Program.cs
@@ -2,15 +2,18 @@
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ.
 Console.WriteLine("Введите число");
 int num = int.Parse(Console.ReadLine());
-int amount = num.ToString().Length;
-if (amount < 3)
+num = Math.Abs(num);
+//int amount = num.ToString().Length;
+//if (amount < 3)
+if (num < 100)
 {
     Console.WriteLine("нет третьей цифры");
 }
 else
-{ int c = 1;
-    for (int i = amount; i > 3; i--)
-    { c = c * 10; }
-    int thirdnum = (num / c) % 10;
+{ while(num>999) {num=num/10;}
+    // int c = 1;
+    // for (int i = amount; i > 3; i--)
+    //{ c = c * 10; }
+    int thirdnum = num % 10;
     Console.WriteLine(thirdnum);
 }
